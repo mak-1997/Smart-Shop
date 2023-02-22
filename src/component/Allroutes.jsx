fa61dbaf13d3d3f7ed8f59d19f06f10720ef33d8
Ojas <ojasbendale18200@gmail.com>
2023-02-22 21:18:40 +0530

    day-2

--- src/component/Allroutes.jsx
+++ src/component/Allroutes.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
-
-
+import Admin from "./Admin";
 
 const Allroutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/admin" element={<Admin />} />
     </Routes>
   );
 };
--- src/redux/Auth/auth.actionTypes.js
+++ src/redux/Auth/auth.actionTypes.js
@@ -1,34 +1,15 @@
-// user add
-export const ADD_USER_LOADING = "user/add/laoding";
-export const ADD_USER_SUCCESS = "user/add/success";
-export const ADD_USER_ERROR = "user/add/error";
-//get user
-export const GET_USER_LOADING = "user/get/laoding";
-export const GET_USER_SUCCESS = "user/get/success";
-export const GET_USER_ERROR = "user/get/error";
+// Register
 
-// REMOVE user
-export const REMOVE_USER_LOADING = "user/remove/laoding";
-export const REMOVE_USER_SUCCESS = "user/remove/success";
-export const REMOVE_USER_ERROR = "user/remove/error";
+export const LOADING = "loading";
+export const ERROR = "error";
+export const REGISTER_SUCCESS = "user/success";
 
-// set user data after login
-export const LOGIN_USER_LOADING = "login/userdata/laoding";
-export const LOGIN_USER_SUCCESS = "login/userdata/success";
-export const LOGIN_USER_ERROR = "login/userdata/error";
+// Get User;
+export const GET_USER = "getuser";
 
-export const LOGOut_USER_LOADING = "logOut/userdata/laoding";
-export const LOGOut_USER_SUCCESS = "logOut/userdata/success";
-export const LOGOut_USER_ERROR = "logOut/userdata/error";
+// Check Login
+export const Login = "Login";
+export const LOGOUT = "logout";
 
-// Admin add
-export const ADD_Admin_LOADING = "Admin/add/laoding";
-export const ADD_Admin_SUCCESS = "Admin/add/success";
-export const ADD_Admin_ERROR = "Admin/add/error";
-// Get Admin
-export const Get_Admin_LOADING = "Admin/Get/laoding";
-export const Get_Admin_SUCCESS = "Admin/Get/success";
-export const Get_Admin_ERROR = "Admin/Get/error";
-
-//delete Admin
-export const DELETE_ADMIN = "admin/delete";
+// Admin Login
+export const ADMIN = "Admin/Login";
